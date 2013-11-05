298b8a3a7f755e58d76104555e8a04f6c57673c3
gtmel <gauthierthibault@live.fr>
2013-11-05 13:30:02 +0100

    Ajout de la méthode run() de la classe ThreadListenerUDP

--- workspace/projet_msn/src/clientServer/ThreadListenerUDP.java
+++ workspace/projet_msn/src/clientServer/ThreadListenerUDP.java
@@ -1,5 +1,30 @@
 package clientServer;
 
-public class ThreadListenerUDP {
+import java.net.*;
+
+public class ThreadListenerUDP extends Thread {
+
+	final static int port = 30003;
+	final static int size = 1024;
+	final static byte buffer[] = new byte[size];
+	private DatagramSocket socket;
+
+	public void run() 
+	{
+		try 
+		{
+			socket = new DatagramSocket(port);
+			while (true) 
+			{
+				DatagramPacket data = new DatagramPacket(buffer, buffer.length);
+				socket.receive(data);
+				System.out.println(data.getAddress());
+				socket.send(data);
+			}
+		} catch (Exception e) 
+		{
+			// TODO: handle exception
+		}
+	}
 
 }
