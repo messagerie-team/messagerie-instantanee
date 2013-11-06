d556dd3668e1d681835c9a82324214fa29d52b1a
MickaelFardilha <mickael.fardilha@gmail.com>
2013-11-06 04:26:59 -0800

    ajout version 1.0 de l interface graphique et de la doc sous forme d image

--- workspace/projet_msn/src/userInterface/ClientUI.java
+++ workspace/projet_msn/src/userInterface/ClientUI.java
@@ -1,22 +1,101 @@
-/**
- * 
- */
 package userInterface;
 
-/**
- * @author Mickael
- *
- */
-public class ClientUI 
-{
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JTextPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ClientUI {
+
+	private JFrame frame;
 
 	/**
-	 * @param args
+	 * Launch the application.
 	 */
-	public static void main(String[] args) 
-	{
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ClientUI window = new ClientUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
+	/**
+	 * Create the application.
+	 */
+	public ClientUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 800, 700);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 182, 662);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(181, 0, 603, 662);
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+
+		JButton btnEnvoyer = new JButton("Envoyer");
+		btnEnvoyer.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0)
+			{
+
+				//TODO action bouton envoyer
+
+			}
+		});
+		btnEnvoyer.setBounds(504, 520, 89, 60);
+		panel_1.add(btnEnvoyer);
+
+		JButton btnQuitter = new JButton("Quitter");
+		btnQuitter.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				//TODO action bouton quitter
+			}
+		});
+		btnQuitter.setBounds(504, 591, 89, 60);
+		panel_1.add(btnQuitter);
+
+		JScrollPane scrollPaneSaisie = new JScrollPane();
+		scrollPaneSaisie.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPaneSaisie.setBounds(10, 520, 482, 131);
+		panel_1.add(scrollPaneSaisie);
+
+		JTextArea textAreaSaisie = new JTextArea();
+		scrollPaneSaisie.setViewportView(textAreaSaisie);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(10, 11, 583, 496);
+		panel_1.add(scrollPane);
+
+		JTextPane textPane = new JTextPane();
+		scrollPane.setViewportView(textPane);
+	}
 }
