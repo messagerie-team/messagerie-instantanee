54870d7be51e6223f48594c905d72d82c977a5fe
MickaelFardilha <mickael.fardilha@gmail.com>
2013-11-18 03:15:28 -0800

    modification interface user

--- workspace/projet_msn/src/userInterface/ClientUI.java
+++ workspace/projet_msn/src/userInterface/ClientUI.java
@@ -1,49 +1,38 @@
 package userInterface;
 
-import java.awt.Color;
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Vector;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JTextArea;
-import javax.swing.ScrollPaneConstants;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ListSelectionModel;
+import java.awt.Dimension;
+import java.awt.Component;
 
-import clientServer.Client;
-
-import dataLink.ProtocolUDP;
-
-public class ClientUI
+public class ClientUI extends JFrame 
 {
 
-	private JFrame frame;
-	// private final static int size = 60000;
-	// private final static byte buffer[] = new byte[size];
-	// private static ProtocolUDP protocol = new ProtocolUDP();
-	private Client client;
-	private JTextArea textAreaSaisie;
-	private JTextArea textAreaRecu;
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args)
-	{
-		EventQueue.invokeLater(new Runnable()
-		{
-			public void run()
-			{
-				try
-				{
-					ClientUI window = new ClientUI();
-					window.frame.setVisible(true);
-				} catch (Exception e)
-				{
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ClientUI frame = new ClientUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
@@ -51,93 +40,77 @@ public class ClientUI
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
-	public ClientUI()
-	{
-		this.client = new Client("toto2", 3001);
-		initialize();
-		this.client.registerToServer();
-		this.client.askListToServer();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize()
-	{
-		frame = new JFrame();
-		frame.getContentPane().setForeground(Color.WHITE);
-		frame.setResizable(false);
-		frame.setBounds(100, 100, 800, 700);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-
-		JPanel panelTchat = new JPanel();
-		panelTchat.setBounds(181, 0, 603, 662);
-		frame.getContentPane().add(panelTchat);
-		panelTchat.setLayout(null);
-
-		JButton btnEnvoyer = new JButton("Envoyer");
-		btnEnvoyer.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent arg0)
-			{
-				String textToSend = textAreaSaisie.getText();
-				System.out.println(textToSend);
-				// protocol.sendMessage(textToSend);
-				client.sendMessageToClient(textToSend, client.getDialogs().firstElement().getIdDialog());
-				textAreaRecu.append("me<" + textToSend + "\n");
-				textAreaSaisie.setText("");
+	public ClientUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelBas = new JPanel();
+		contentPane.add(panelBas, BorderLayout.SOUTH);
+		panelBas.setLayout(new BorderLayout(0, 0));
+		
+		Box hbBas = Box.createHorizontalBox();
+		panelBas.add(hbBas, BorderLayout.CENTER);
+		
+		JTextArea textAreaSaisie = new JTextArea();
+		hbBas.add(textAreaSaisie);
+		
+		Component hsRightSaisie = Box.createHorizontalStrut(20);
+		hsRightSaisie.setPreferredSize(new Dimension(5, 0));
+		hbBas.add(hsRightSaisie);
+		
+		Box vbBouton = Box.createVerticalBox();
+		hbBas.add(vbBouton);
+		
+		JButton btnValider = new JButton("Valider");
+		btnValider.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		btnEnvoyer.setBounds(504, 520, 89, 60);
-		panelTchat.add(btnEnvoyer);
-
+		vbBouton.add(btnValider);
+		
 		JButton btnQuitter = new JButton("Quitter");
-		btnQuitter.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				// TODO action bouton quitter
-				client.unregisterToServer();
-				client.getThreadListenerUDP().stopThread();
+		btnQuitter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnQuitter.setBounds(504, 591, 89, 60);
-		panelTchat.add(btnQuitter);
-
-		JScrollPane scrollPaneSaisie = new JScrollPane();
-		scrollPaneSaisie.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPaneSaisie.setBounds(10, 520, 482, 131);
-		panelTchat.add(scrollPaneSaisie);
-
-		textAreaSaisie = new JTextArea();
-		scrollPaneSaisie.setViewportView(textAreaSaisie);
-
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
-		scrollPane.setBounds(10, 11, 583, 496);
-		panelTchat.add(scrollPane);
-
-		textAreaRecu = new JTextArea();
-		scrollPane.setViewportView(textAreaRecu);
-
-		JScrollPane scrollPaneClient = new JScrollPane();
-		scrollPaneClient.setBounds(10, 11, 172, 640);
-		frame.getContentPane().add(scrollPaneClient);
-
-		Vector<String> clientVector = new Vector<String>();
-		clientVector.add(new String("Micka"));
-		clientVector.add(new String("Thibault"));
-		clientVector.add(new String("Dorian"));
-		clientVector.add(new String("Raph"));
-		JList listClients = new JList(clientVector);
-		scrollPaneClient.setViewportView(listClients);
+		vbBouton.add(btnQuitter);
+		
+		JPanel panelHaut = new JPanel();
+		contentPane.add(panelHaut, BorderLayout.CENTER);
+		panelHaut.setLayout(new BorderLayout(0, 0));
+		
+		Box hbHaut = Box.createHorizontalBox();
+		panelHaut.add(hbHaut, BorderLayout.CENTER);
+		
+		JPanel panelList = new JPanel();
+		hbHaut.add(panelList);
+		panelList.setLayout(new BorderLayout(0, 0));
+		
+		JList list = new JList();
+		panelList.add(list, BorderLayout.CENTER);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {"Micka", "Thibault", "Dorian", "Raph"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		
+		Component hsRightList = Box.createHorizontalStrut(20);
+		hsRightList.setPreferredSize(new Dimension(5, 0));
+		hbHaut.add(hsRightList);
+		
+		JTextArea textAreaDialog = new JTextArea();
+		hbHaut.add(textAreaDialog);
 	}
 
-	public void addMessage(String msg)
-	{
-		textAreaRecu.append(msg + "\n");
-	}
 }
