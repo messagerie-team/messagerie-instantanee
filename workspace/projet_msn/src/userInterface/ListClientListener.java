e8fce2b9b83c0632accb68fe2adfdafbf647dde1
ediraphael <raphael.pillie@gmail.com>
2013-11-21 21:41:52 +0100

    AJout d'un listener pour la list des clients

---
+++ workspace/projet_msn/src/userInterface/ListClientListener.java
@@ -0,0 +1,47 @@
+package userInterface;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class ListClientListener implements MouseListener
+{
+	@Override
+	public void mouseClicked(MouseEvent e)
+	{
+		if (e.getClickCount() == 2)
+		{
+			System.out.println("dbclick");
+			ClientListData clientList = ClientServerUI.listTest.getSelectedValue();
+			ClientServerUI.client.askClientConnectionToServer(clientList.getKey());
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
