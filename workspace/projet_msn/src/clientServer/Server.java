90f3c9945f4db728bf603428b01756c0f94f9e2c
geeknbar <dorian.coffinet@gmail.com>
2013-12-02 23:38:40 +0100

    Génération de la javadoc de la classe Serveur, correction dans le nom d'un auteur

--- workspace/projet_msn/src/clientServer/Server.java
+++ workspace/projet_msn/src/clientServer/Server.java
@@ -29,7 +29,7 @@ import dataLink.ProtocolUDP;
 /**
  * Classe permettant de représenter un serveur. Extends de la classe
  *           AbstractClientServer.
- * @author Dorian, Mickaël, Raphaël, Thibaultl
+ * @author Dorian, Mickaël, Raphaël, Thibault
  * @see AbstractClientServer
  */
 public class Server extends AbstractClientServer
