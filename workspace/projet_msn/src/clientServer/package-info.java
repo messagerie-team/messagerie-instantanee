fc793b82a786e329589937e252dfe054941fe9ae
geeknbar <dorian.coffinet@gmail.com>
2013-12-03 14:26:52 +0100

    ajour de description pour le package clientServer

---
+++ workspace/projet_msn/src/clientServer/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Le package clientServer permet de représenter le modèle de l'application.
+ * Les entités Client / Serveur.
+ * Les différents threads de communications / écoutes
+ */
+package clientServer;
