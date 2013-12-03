ea2ab10526643fe02b09243af1d9789651f32105
ediraphael <raphael.pillie@gmail.com>
2013-12-03 14:59:30 +0100

    Retrait d'un warning

--- workspace/projet_msn/src/userInterface/AddClientUI.java
+++ workspace/projet_msn/src/userInterface/AddClientUI.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.Vector;
