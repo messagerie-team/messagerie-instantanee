547dbd6233a7272bed2cf113b348163bc5732034
ediraphael <raphael.pillie@gmail.com>
2013-12-15 18:14:59 +0100

    Suppression des warning de la class FileTransfer

--- workspace/projet_msn/src/network/FileTransfer.java
+++ workspace/projet_msn/src/network/FileTransfer.java
@@ -1,7 +1,5 @@
 package network;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
