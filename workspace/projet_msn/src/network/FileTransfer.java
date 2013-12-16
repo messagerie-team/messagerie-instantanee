5cdf7e4d202e5af945923a194170da8515d48cf7
ediraphael <raphael.pillie@gmail.com>
2013-12-16 13:21:00 +0100

    Ajout de l'envoie du nom de fichier, indentation du code

--- workspace/projet_msn/src/network/FileTransfer.java
+++ workspace/projet_msn/src/network/FileTransfer.java
@@ -1,12 +1,17 @@
 package network;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 /**
  * Classe représentant le transfert de fichier
@@ -25,11 +30,14 @@ public class FileTransfer
 	public static void send(OutputStream os, String file) throws Exception
 	{
 		File fileF = new File(file);
-		String[] pathFile=file.split("/");
-		String nameFile = pathFile[pathFile.length-1];
-		System.out.println("Nom du fichier : "+ nameFile);
+		String[] pathFile = file.split("/");
+		String nameFile = pathFile[pathFile.length - 1];
+		System.out.println("Nom du fichier : " + nameFile);
 		
-		
+		PrintWriter print = new PrintWriter(new BufferedWriter(new OutputStreamWriter(os)), true);
+		print.println(nameFile);
+		print.flush();
+		print.close();
 		
 		int fileSize = (int) fileF.length();
 		FileInputStream fis = new FileInputStream(fileF);
@@ -70,13 +78,21 @@ public class FileTransfer
 		// int sizeAvailable = receiveSocket.getReceiveBufferSize();
 		// System.out.println("Available Size Server Socket: " + sizeAvailable);
 		// Ouverture des Input et Output du fichier à enregistrer
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		String fichier = reader.readLine();
+		reader.close();
+		System.out.println(fichier);
+		
+		
 		DataInputStream dis = new DataInputStream(is);
 		// Reception du fichier
-		File receivedFile = new File(folder);
+		File receivedFile = new File(fichier);
 		FileOutputStream fos = new FileOutputStream(receivedFile);
 
+		// attente available devienne different de zero
 		while (dis.available() == 0)
-			; // attente available devienne different de zero
+			;
 
 		// Attente de 10
 		Thread.sleep(10);
